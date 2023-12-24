b1fe7284225ba1a5f8350a30f23fa4c95e3593f5
Kent Edoloverio <kentedoloverio25@gmail.com>
2023-12-24 15:04:56 +0800

    migrating to gpt 3.5 turbo

--- engine.py
+++ engine.py
@@ -1,21 +1,26 @@
-import requests
-import json
-import openai
 import os
+from openai import OpenAI
+from dotenv import load_dotenv
 
-openai.api_key = os.environ['OPENAI_API_KEY']
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.environ['OPENAI_API_KEY']
+)
 
 def openai_process_message(user_message):
     # Set the prompt for OpenAI Api
     prompt = "\"Act like a personal assistant. You can respond to questions, translate sentences, summarize news, and give recommendations. " + user_message + "\""
 
     # Call the OpenAI Api to process our prompt
-    response = openai.Completion.create(
-        engine="text-davinci-003",  # You can use a different engine based on your needs
-        prompt=prompt,
-        max_tokens=100  # Adjust max_tokens as needed
+    response = client.chat.completions.create(
+        model='gpt-3.5-turbo-1106',
+        messages=[
+            {'role': 'user', 'content': prompt}
+        ],
+        temperature=0
     )
-    # Parse the response to get the response text for our prompt
-    generated_text = response['choices'][0]['text']
-    return generated_text
 
+    # Parse the response to get the response text for our prompt
+    generated_text = response.choices[0].message.content
+    return generated_text
